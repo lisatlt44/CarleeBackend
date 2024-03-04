52096eaffc76c9bbd96488da4eed1816eabb4c7c
lisatlt44 <lisa.tallet1@gmail.com>
2024-03-04 22:15:59 +0100

    test bdd

--- app/Models/UserTest.php
+++ app/Models/UserTest.php
@@ -1,16 +1,11 @@
 <?php
 
-namespace App\Models;
+namespace App\Model;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class UserTest extends Model
 {
-    use HasFactory;
-
-    const CREATED_AT = 'created_at';
-    const UPDATED_AT = 'updated_at';
     protected $table = 'user';
     protected $connection = 'mysql';
     protected $primaryKey = 'id_user';
