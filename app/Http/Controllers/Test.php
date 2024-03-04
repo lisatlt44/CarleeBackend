11e920e3363be0f0149f367f0f183e4baa68caf4
lisatlt44 <lisa.tallet1@gmail.com>
2024-03-04 22:28:25 +0100

    test bdd

--- app/Http/Controllers/Test.php
+++ app/Http/Controllers/Test.php
@@ -2,9 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Controllers\Controller;
 use App\Model\UserTest;
 
-class Test extends BaseController
+class Test extends Controller
 {
   public function test() {
     $user = UserTest::all();
