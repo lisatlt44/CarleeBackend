9ca30499b14d25591cc721c6ff48b0c4d8faecf8
lisatlt44 <lisa.tallet1@gmail.com>
2024-03-13 21:55:18 +0100

    add class

--- app/Http/Controllers/CarPictureController.php
+++ app/Http/Controllers/CarPictureController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Models\Car;
+use Illuminate\Support\Facades\Storage;
 use App\Models\CarPicture;
 
 class CarPictureController extends Controller
