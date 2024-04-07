68a3415927cc5ea74d36db614f0382b1bed62ef0
lisatlt44 <lisa.tallet1@gmail.com>
2024-04-07 17:42:09 +0200

    add validator

--- app/Http/Controllers/DocumentController.php
+++ app/Http/Controllers/DocumentController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\Models\Document;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Str;
+use Validator;
 
 class DocumentController extends Controller
 {
