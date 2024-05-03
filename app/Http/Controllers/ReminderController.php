52a66f00455800a8050c4c3a72129b896c6ee097
lisatlt44 <lisa.tallet1@gmail.com>
2024-05-03 15:38:05 +0200

    update controller reminder

--- app/Http/Controllers/ReminderController.php
+++ app/Http/Controllers/ReminderController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\Indicator;
+use App\Models\Reminder;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Str;
 use Validator;
